686f3a541b566a59fbfee80c0aca11cb6bb45102
Isaac738 <54778430+Isaac738@users.noreply.github.com>
2022-06-07 08:42:47 -0400

    Update breakinfinity.js

--- breakinfinity.js
+++ breakinfinity.js
@@ -1,3 +1,8 @@
+//The function below is used to make the log to decimal be nicer
+function mod(a, b) {
+  return a - Math.floor(a / b) * b
+}
+
 function add(a, b) {
   if(a > b) {
     return a + Math.log(Math.pow(10, b - a) + 1);
@@ -18,10 +23,10 @@ function decimal(log, pl=3, shsci=6, shnorm=false) {
   if(log < shsci) {
     return Math.round(Math.pow(10, log + pl)) / Math.pow(10, pl);
   } else if(log < Math.pow(10, shsci) {
-    return Math.round(Math.pow(10, log - Math.floor(log) + pl)) / Math.pow(10, pl) + "e" + Math.floor();
+    return Math.round(Math.pow(10, mod(log, 1) + pl)) / Math.pow(10, pl) + "e" + Math.floor(log);
   } else if(shnorm) {
-    
+    return Math.round(Math.pow(10, mod(log, 1) + pl)) / Math.pow(10, pl) + "e" + Math.round(Math.floor(log) * Math.pow(pl) / Math.pow(Math.floor(Math.log(log)))) / Math.pow(10, pl) + "e" + Math.floor(Math.log(log));
   } else {
-  
+    return "e" + Math.round(Math.floor(log) * Math.pow(pl) / Math.pow(Math.floor(Math.log(log)))) / Math.pow(10, pl) + "e" + Math.floor(Math.log(log));
   }
 }
